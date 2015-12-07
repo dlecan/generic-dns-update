ba8770d31f972df2d465c3646f55a8290d173e09
Damien Lecan <dev@dlecan.com>
2015-12-07 11:50:38 +0100

    Build ARM package and deploy it on Packagecloud

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,6 +4,8 @@ set -ev
 
 source ./scripts/config.sh
 
+## AMD64 binary
+
 OS="linux"
 ARCH="amd64"
 BUILD_BIN_FILE="$(pwd)/target/release/gdu"
@@ -14,6 +16,27 @@ OUTPUT_DIR="$(pwd)/pkg/"
 mkdir -p $OUTPUT_DIR
 
 cargo build --release
+
+fpm \
+  -s dir \
+  -t deb \
+  -n $PKG_NAME \
+  -p $OUTPUT_DIR \
+  -v $VERSION \
+  -a $ARCH \
+  --vendor $VENDOR \
+  $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+
+## ARMv6 binary for Raspbian
+
+OS="linux"
+ARCH="armhf"
+BUILD_BIN_FILE="$(pwd)/target/arm-unknown-linux-gnueabihf/release/gdu"
+
+docker run -it --rm \
+  -v $(pwd):/source \
+  dlecan/rust-x86_64-armv6 \
+  cargo build --release --target arm-unknown-linux-gnueabihf
 
 fpm \
   -s dir \
