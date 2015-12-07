85009103e6140020e1eb0e4a308d1ca1ab7fc4f4
Damien Lecan <dev@dlecan.com>
2015-12-07 11:59:29 +0100

    Build packages only if they don't exist

--- scripts/package.sh
+++ scripts/package.sh
@@ -13,37 +13,41 @@ PKG_INSTALL_BIN_DIR="/usr/bin/"
 PKG_NAME="$APP"
 OUTPUT_DIR="$(pwd)/pkg/"
 
-mkdir -p $OUTPUT_DIR
+if [ ! -d "$OUTPUT_DIR" ]
+then
+  mkdir -p $OUTPUT_DIR
 
-cargo build --release
+  cargo build --release
 
-fpm \
-  -s dir \
-  -t deb \
-  -n $PKG_NAME \
-  -p $OUTPUT_DIR \
-  -v $VERSION \
-  -a $ARCH \
-  --vendor $VENDOR \
-  $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+  fpm \
+    -s dir \
+    -t deb \
+    -n $PKG_NAME \
+    -p $OUTPUT_DIR \
+    -v $VERSION \
+    -a $ARCH \
+    --vendor $VENDOR \
+    $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
 
-## ARMv6 binary for Raspbian
+  ## ARMv6 binary for Raspbian
 
-OS="linux"
-ARCH="armhf"
-BUILD_BIN_FILE="$(pwd)/target/arm-unknown-linux-gnueabihf/release/gdu"
+  OS="linux"
+  ARCH="armhf"
+  BUILD_BIN_FILE="$(pwd)/target/arm-unknown-linux-gnueabihf/release/gdu"
 
-docker run -it --rm \
-  -v $(pwd):/source \
-  dlecan/rust-x86_64-armv6 \
-  cargo build --release --target arm-unknown-linux-gnueabihf
+  docker run -it --rm \
+    -v $(pwd):/source \
+    dlecan/rust-x86_64-armv6 \
+    cargo build --release --target arm-unknown-linux-gnueabihf
 
-fpm \
-  -s dir \
-  -t deb \
-  -n $PKG_NAME \
-  -p $OUTPUT_DIR \
-  -v $VERSION \
-  -a $ARCH \
-  --vendor $VENDOR \
-  $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+  fpm \
+    -s dir \
+    -t deb \
+    -n $PKG_NAME \
+    -p $OUTPUT_DIR \
+    -v $VERSION \
+    -a $ARCH \
+    --vendor $VENDOR \
+    $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+fi
+
