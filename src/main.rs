c31b2740069ea926039d7e7e46c1b1d18dc6d5b0
Damien Lecan <dev@dlecan.com>
2015-12-12 21:00:55 +0100

    'Get zone_id': done

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,72 @@ extern crate clap;
 extern crate log;
 extern crate env_logger;
 
-use clap::{Arg, App, SubCommand};
+extern crate xmlrpc;
+
+use clap::{Arg, App};
 use std::env;
 use std::io;
 use std::io::prelude::*;
 
-const GANDI_URL_PROD: &'static str = "https://rpc.gandi.net/";
+const GANDI_URL_PROD: &'static str = "https://rpc.gandi.net/xmlrpc/";
 
 trait GandiAPI {
-    fn isRecordAlreadyDeclared(&self, record: &str) -> Option<&str>;
-    fn updateRecord(&self, record: &str, ipAddr: &str);
-    fn createRecord(&self, record: &str, ipAddr: &str);
+    fn init(&mut self, domain: &str);
+    fn is_record_already_declared(&self, record: &str) -> Option<&str>;
+    fn update_record(&self, record: &str, ip_addr: &str);
+    fn create_record(&self, record: &str, ip_addr: &str);
 }
 
 struct GandiAPIImpl<'a> {
   xmlrpc_server: &'a str,
-  apikey: &'a str
+  apikey: &'a str,
+  zone_id: Option<i32>,
+}
+
+impl<'a> GandiAPIImpl<'a> {
+
+    fn new(gandi_url: &'a str, gandi_apikey: &'a str) -> GandiAPIImpl<'a> {
+        GandiAPIImpl {
+            xmlrpc_server: gandi_url,
+            apikey: gandi_apikey,
+            zone_id: None,
+        }
+    }
 }
 
 impl<'a> GandiAPI for GandiAPIImpl<'a> {
-    fn isRecordAlreadyDeclared(&self, record: &str) -> Option<&str> {
+
+    fn init(&mut self, domain: &str) {
+        let client = xmlrpc::Client::new(self.xmlrpc_server);
+        let mut request = xmlrpc::Request::new("domain.info");
+        request = request.argument(&self.apikey.to_string());
+        request = request.argument(&domain.to_string());
+        request = request.finalize();
+
+        let response = client.remote_call(&request).unwrap();
+
+        // TODO: fix this ugly code
+        // Handle errors
+        let zone_id_pos = response.body.find("zone_id").unwrap();
+        let (_, body_end) = response.body.split_at(zone_id_pos);
+        let first_int_markup = body_end.find("<int>").unwrap();
+        let (_, body_end) = body_end.split_at(first_int_markup + "<int>".len());
+        let first_end_int_markup = body_end.find("</int>").unwrap();
+        let (zone_id, _) = body_end.split_at(first_end_int_markup);
+        self.zone_id = zone_id.parse::<i32>().ok();
+
+        debug!("Zone id: {}", self.zone_id.unwrap());
+    }
+
+    fn is_record_already_declared(&self, record: &str) -> Option<&str> {
         unimplemented!();
     }
 
-    fn updateRecord(&self, record: &str, ipAddr: &str) {
+    fn update_record(&self, record: &str, ip_addr: &str) {
         unimplemented!();
     }
 
-    fn createRecord(&self, record: &str, ipAddr: &str) {
+    fn create_record(&self, record: &str, ip_addr: &str) {
         unimplemented!();
     }
 }
@@ -55,8 +93,16 @@ fn main() {
         .get_matches();
 
     // Init logging to DEBUG only if user requires it
-    if matches.is_present("verbose") {
-        env::set_var("RUST_LOG", "DEBUG");
+    match matches.occurrences_of("verbose") {
+        0 => (),
+        1 => {
+                println!("Verbose mode");
+                env::set_var("RUST_LOG", "DEBUG");
+            },
+        2 | _=> {
+                println!("More verbose mode");
+                env::set_var("RUST_LOG", "TRACE");
+            }
     }
     env_logger::init().unwrap();
 
@@ -80,22 +126,21 @@ fn main() {
         Err(error) => println!("error: {}", error),
     }
 
-    let detectedIpAddr = input.trim();
+    let detected_ip_addr = input.trim();
 
-    let gandi_api = GandiAPIImpl {
-      xmlrpc_server: GANDI_URL_PROD,
-      apikey: apikey
-    };
+    let mut gandi_api = GandiAPIImpl::new(GANDI_URL_PROD, apikey);
 
-    let maybeChecked = gandi_api.isRecordAlreadyDeclared(record_name);
+    gandi_api.init(domain);
 
-    match maybeChecked {
-        Some(ipAddr) => {
-            if ipAddr != detectedIpAddr {
-                gandi_api.updateRecord(record_name, ipAddr);
-            }
-        }
-        None => gandi_api.createRecord(record_name, detectedIpAddr)
-    }
+    // let maybeChecked = gandi_api.isRecordAlreadyDeclared(record_name);
+
+    // match maybeChecked {
+    //     Some(ipAddr) => {
+    //         if ipAddr != detectedIpAddr {
+    //             gandi_api.updateRecord(record_name, ipAddr);
+    //         }
+    //     }
+    //     None => gandi_api.createRecord(record_name, detectedIpAddr)
+    // }
 
 }
