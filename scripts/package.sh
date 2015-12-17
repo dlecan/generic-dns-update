501cfb893b9cc36da66f4733b0d9c080ce3cfdc9
Damien Lecan <dev@dlecan.com>
2015-12-17 21:15:57 +0100

    Build ARM package only with arm targets in build matrix

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,7 +9,7 @@ source ./scripts/config.sh
 OS="linux"
 PKG_INSTALL_BIN_DIR="/usr/bin/"
 PKG_NAME="$APP"
-OUTPUT_DIR="$(pwd)/pkg/"
+OUTPUT_DIR="$(pwd)/pkg/$ARCH"
 
 DEB_VERSION="$CARGO_VERSION.b$TRAVIS_BUILD_NUMBER-1git+$GIT_VERSION"
 RPM_VERSION="$CARGO_VERSION.b$TRAVIS_BUILD_NUMBER_1git+$GIT_VERSION"
@@ -20,54 +20,65 @@ then
 else
   mkdir -p $OUTPUT_DIR
 
-  cargo build --release
+  gem install fpm
 
-  ## 64 bits deb package
-  ARCH="amd64"
-  BUILD_BIN_FILE="$(pwd)/target/release/gdu"
+  if [ "$ARCH" = "x86_64" ]
+  then
 
-  fpm \
-    -s dir \
-    -t deb \
-    -n $PKG_NAME \
-    -p $OUTPUT_DIR \
-    -v $DEB_VERSION \
-    -a $ARCH \
-    --vendor $VENDOR \
-    $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+    cargo build --release
 
-  ## 64 bits rpm package
-  ARCH="x86_64"
-  BUILD_BIN_FILE="$(pwd)/target/release/gdu"
+    ## 64 bits deb package
+    ARCH="amd64"
+    BUILD_BIN_FILE="$(pwd)/target/release/gdu"
 
-  fpm \
-    -s dir \
-    -t rpm \
-    -n $PKG_NAME \
-    -p $OUTPUT_DIR \
-    -v $RPM_VERSION \
-    -a $ARCH \
-    --vendor $VENDOR \
-    $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+    fpm \
+      -s dir \
+      -t deb \
+      -n $PKG_NAME \
+      -p $OUTPUT_DIR \
+      -v $DEB_VERSION \
+      -a $ARCH \
+      --vendor $VENDOR \
+      $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
 
-  ## ARMv6 binary for Raspbian
+    ## 64 bits rpm package
+    ARCH="x86_64"
+    BUILD_BIN_FILE="$(pwd)/target/release/gdu"
 
-  ARCH="armhf"
-  BUILD_BIN_FILE="$(pwd)/target/arm-unknown-linux-gnueabihf/release/gdu"
+    fpm \
+      -s dir \
+      -t rpm \
+      -n $PKG_NAME \
+      -p $OUTPUT_DIR \
+      -v $RPM_VERSION \
+      -a $ARCH \
+      --vendor $VENDOR \
+      $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
 
-  docker run -it --rm \
-    -v $(pwd):/source \
-    dlecan/rust-x86_64-armv6 \
-    cargo build --release --target arm-unknown-linux-gnueabihf
+  elif [ "$ARCH" = "armv6" ]
+  then
+    ## ARMv6 binary for Raspbian
 
-  fpm \
-    -s dir \
-    -t deb \
-    -n $PKG_NAME \
-    -p $OUTPUT_DIR \
-    -v $DEB_VERSION \
-    -a $ARCH \
-    --vendor $VENDOR \
-    $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+    ARCH="armhf"
+    BUILD_BIN_FILE="$(pwd)/target/arm-unknown-linux-gnueabihf/release/gdu"
+
+    docker run -it --rm \
+      -v $(pwd):/source \
+      dlecan/rust-x86_64-armv6 \
+      cargo build --release --target arm-unknown-linux-gnueabihf
+
+    fpm \
+      -s dir \
+      -t deb \
+      -n $PKG_NAME \
+      -p $OUTPUT_DIR \
+      -v $DEB_VERSION \
+      -a $ARCH \
+      --vendor $VENDOR \
+      $BUILD_BIN_FILE=$PKG_INSTALL_BIN_DIR
+
+  else
+    echo "Unknown architecture!"
+  fi
 fi
 
