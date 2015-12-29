bef5a0fe7eb0f7aa97270dce4942ec6bc74f5786
Damien Lecan <dev@dlecan.com>
2015-12-29 23:44:07 +0100

    Optimized use of Error

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,28 @@
 use std::error::Error as StdError;
 use std::fmt;
-use std::io;
+use std::io::Error as IoError;
 use std::net::AddrParseError;
+
+use self::Error::{
+    Io,
+    AddrParse,
+};
+
+/// Result type often returned from methods
+pub type Result<T> = ::std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    Io(io::Error),
+    Io(IoError),
     AddrParse(AddrParseError)
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Error::Io(ref err) => err.fmt(f),
-            Error::AddrParse(ref err) => err.fmt(f),
-//            Error::Another => write!(f, "No matching cities with a \
+            Io(ref err) => err.fmt(f),
+            AddrParse(ref err) => err.fmt(f),
+//            Another => write!(f, "No matching cities with a \
 //                                             population were found."),
         }
     }
@@ -23,21 +31,21 @@ impl fmt::Display for Error {
 impl StdError for Error {
     fn description(&self) -> &str {
         match *self {
-            Error::Io(ref err) => err.description(),
-            Error::AddrParse(ref err) => err.description(),
-//            Error::Another => "not found",
+            Io(ref err) => err.description(),
+            AddrParse(ref err) => err.description(),
+//            Another => "not found",
         }
     }
 }
 
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::Io(err)
+impl From<IoError> for Error {
+    fn from(err: IoError) -> Error {
+        Io(err)
     }
 }
 
 impl From<AddrParseError> for Error {
     fn from(err: AddrParseError) -> Error {
-        Error::AddrParse(err)
+        AddrParse(err)
     }
 }
--- src/myip.rs
+++ src/myip.rs
@@ -1,4 +1,4 @@
-use error::Error;
+use error::Result;
 use ip::IpAddr;
 use std::io;
 use std::io::prelude::*;
@@ -6,13 +6,13 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str::FromStr;
 
 pub trait MyIPAddressProvider<'a> {
-    fn get_my_ip_addr(&self) -> Result<IpAddr, Error>;
+    fn get_my_ip_addr(&self) -> Result<IpAddr>;
 }
 
 pub struct StdinIpProvider;
 
 impl<'a> MyIPAddressProvider<'a> for StdinIpProvider {
-    fn get_my_ip_addr(&self) -> Result<IpAddr, Error> {
+    fn get_my_ip_addr(&self) -> Result<IpAddr> {
         let mut input = String::new();
 
         try!(io::stdin().read_line(&mut input));
