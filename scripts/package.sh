2ef9a4252e16daa5945893ce91c5290996758313
Damien Lecan <dev@dlecan.com>
2016-01-09 07:22:01 +0100

    Reuse cache for build of ARM package with Docker

--- scripts/package.sh
+++ scripts/package.sh
@@ -64,6 +64,8 @@ else
 
     docker run -it --rm \
       -v $(pwd):/source \
+      -v ~/.cargo/git:/root/.cargo/git \
+      -v ~/.cargo/registry:/root/.cargo/registry \
       dlecan/rust-x86_64-armv6:stable \
       cargo build --release --target arm-unknown-linux-gnueabihf
 
