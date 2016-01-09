4e926e24c6698fee06515b4a5fe5502b4539b896
Damien Lecan <dev@dlecan.com>
2016-01-09 07:08:09 +0100

    Reuse local cargo cache with Docker ARM build

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,6 +17,8 @@ then
 
   docker run -it --rm \
     -v $(pwd):/source \
+    -v ~/.cargo/git:/root/.cargo/git \
+    -v ~/.cargo/registry:/root/.cargo/registry \
     dlecan/rust-x86_64-armv6:stable \
     cargo build --verbose --target arm-unknown-linux-gnueabihf
 
