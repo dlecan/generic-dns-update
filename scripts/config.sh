d36ba13f996929b724edb93f2a8007eccc06496d
Damien Lecan <dev@dlecan.com>
2016-08-12 18:15:54 +0200

    Better version detection for package build

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,5 +3,5 @@
 export APP='generic-dns-update'
 export VENDOR='dlecan.com'
 
-export CARGO_VERSION=`grep -m 1 "version = \"[0-9.]*\"" Cargo.toml | sed -n 's/version = "\([0-9.]*\)"/\1/p'`
+export CARGO_VERSION=`grep -m 1 "version = \"[0-9a-z.-]*\"" Cargo.toml | sed -n 's/version = "\([0-9a-z.-]*\)"/\1/p'`
 export GIT_VERSION=${TRAVIS_COMMIT:0:6}
